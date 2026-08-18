6dca9242c215418ba1ae82de96bb0012c028be1a
afterburn25 <afterburn32@gmail.com>
2026-08-18 15:31:34 -0500

    Register private community routes

---
+++ app/Providers/CommunityServiceProvider.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Providers;
+
+use App\Http\Controllers\Member\CommunityController;
+use App\Http\Controllers\Member\LiveChatController;
+use App\Http\Controllers\Member\MessageController;
+use App\Http\Middleware\EnsureTenantMember;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+
+final class CommunityServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        Route::middleware(['web', 'auth', EnsureTenantMember::class])->group(function (): void {
+            Route::get('/community', [CommunityController::class, 'index'])->name('community.index');
+            Route::post('/community/posts', [CommunityController::class, 'store'])->middleware('throttle:20,1')->name('community.posts.store');
+            Route::delete('/community/posts/{post}', [CommunityController::class, 'destroy'])->name('community.posts.destroy');
+            Route::patch('/community/posts/{post}/pin', [CommunityController::class, 'pin'])->name('community.posts.pin');
+            Route::post('/community/posts/{post}/comments', [CommunityController::class, 'comment'])->middleware('throttle:40,1')->name('community.comments.store');
+            Route::delete('/community/comments/{comment}', [CommunityController::class, 'destroyComment'])->name('community.comments.destroy');
+            Route::post('/community/posts/{post}/reaction', [CommunityController::class, 'react'])->middleware('throttle:60,1')->name('community.reactions.store');
+            Route::delete('/community/posts/{post}/reaction', [CommunityController::class, 'removeReaction'])->name('community.reactions.destroy');
+
+            Route::get('/community/chat', [LiveChatController::class, 'index'])->name('community.chat');
+            Route::get('/community/chat/poll', [LiveChatController::class, 'poll'])->middleware('throttle:120,1')->name('community.chat.poll');
+            Route::post('/community/chat', [LiveChatController::class, 'store'])->middleware('throttle:60,1')->name('community.chat.store');
+            Route::delete('/community/chat/{message}', [LiveChatController::class, 'destroy'])->name('community.chat.destroy');
+
+            Route::get('/messages', [MessageController::class, 'index'])->name('messages.index');
+            Route::post('/messages/start', [MessageController::class, 'start'])->middleware('throttle:30,1')->name('messages.start');
+            Route::get('/messages/{conversation}', [MessageController::class, 'show'])->name('messages.show');
+            Route::get('/messages/{conversation}/poll', [MessageController::class, 'poll'])->middleware('throttle:120,1')->name('messages.poll');
+            Route::post('/messages/{conversation}', [MessageController::class, 'store'])->middleware('throttle:60,1')->name('messages.store');
+            Route::patch('/messages/{conversation}/mute', [MessageController::class, 'mute'])->name('messages.mute');
+        });
+    }
+}
