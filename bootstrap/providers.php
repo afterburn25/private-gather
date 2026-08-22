31e197b4a1d8be6b5391ccc3e70af82a58e14d76
afterburn25 <afterburn32@gmail.com>
2026-08-22 03:29:35 -0500

    repro: register live hosted discovery provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,6 +3,7 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\CommunityServiceProvider::class,
+    App\Providers\HostedDiscoveryRevenueServiceProvider::class,
     App\Providers\SelfHostedManagementServiceProvider::class,
     App\Providers\EventWaitlistServiceProvider::class,
     App\Providers\PrivacyAdministrationServiceProvider::class,
