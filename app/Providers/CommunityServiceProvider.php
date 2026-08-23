f5b18d77dcb4f0f440586da8f4ca615395143acd
afterburn25 <afterburn32@gmail.com>
2026-08-22 20:49:07 -0500

    Register redesign group community routes

--- app/Providers/CommunityServiceProvider.php
+++ app/Providers/CommunityServiceProvider.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Providers;
 
 use App\Http\Controllers\Member\CommunityController;
+use App\Http\Controllers\Member\GroupController;
 use App\Http\Controllers\Member\LiveChatController;
 use App\Http\Controllers\Member\MessageController;
 use App\Http\Middleware\EnsureTenantMember;
@@ -25,14 +26,19 @@ final class CommunityServiceProvider extends ServiceProvider
             Route::post('/community/posts/{post}/reaction', [CommunityController::class, 'react'])->middleware('throttle:60,1')->name('community.reactions.store');
             Route::delete('/community/posts/{post}/reaction', [CommunityController::class, 'removeReaction'])->name('community.reactions.destroy');
 
+            Route::get('/community/groups', [GroupController::class, 'index'])->name('community.groups.index');
+            Route::post('/community/groups', [GroupController::class, 'store'])->name('community.groups.store');
+            Route::get('/community/groups/{group}', [GroupController::class, 'show'])->whereNumber('group')->name('community.groups.show');
+            Route::post('/community/groups/{group}/join', [GroupController::class, 'join'])->whereNumber('group')->name('community.groups.join');
+            Route::delete('/community/groups/{group}/leave', [GroupController::class, 'leave'])->whereNumber('group')->name('community.groups.leave');
+            Route::post('/community/groups/{group}/posts', [GroupController::class, 'post'])->whereNumber('group')->middleware('throttle:20,1')->name('community.groups.posts.store');
+            Route::patch('/community/groups/{group}/members/{user}/approve', [GroupController::class, 'approve'])->whereNumber(['group', 'user'])->name('community.groups.members.approve');
+
             Route::get('/community/chat', [LiveChatController::class, 'index'])->name('community.chat');
             Route::get('/community/chat/poll', [LiveChatController::class, 'poll'])->middleware('throttle:120,1')->name('community.chat.poll');
             Route::post('/community/chat', [LiveChatController::class, 'store'])->middleware('throttle:60,1')->name('community.chat.store');
             Route::delete('/community/chat/{message}', [LiveChatController::class, 'destroy'])->name('community.chat.destroy');
 
-            // Existing message index/start/show/store routes remain in web.php for
-            // upgrade compatibility. These extra endpoints add live delivery and
-            // mute controls; MessageController itself enforces tenant membership.
             Route::get('/messages/{conversation}/poll', [MessageController::class, 'poll'])->middleware('throttle:120,1')->name('messages.poll');
             Route::patch('/messages/{conversation}/mute', [MessageController::class, 'mute'])->name('messages.mute');
         });
